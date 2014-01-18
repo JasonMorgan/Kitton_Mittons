9198db20022ec770a1a4c0428a5fe4aadb72b76e
Jason Morgan <jmorgan@f9vs.com>
2014-01-18 16:58:53 -0500

    Completed Test-History

--- Event1/Split-Name.ps1
+++ Event1/Split-Name.ps1
@@ -73,18 +73,60 @@ Param
     (
         [System.Object[]]$array
     )
-$array | Get-Random -Count $array.Count
+process 
+    {
+        $array | Get-Random -Count $array.Count
+    }
 }
 
-Function Combine
+Function New-Team
 {
 Param
     (
         [System.Object[]]$Keys,
         [System.Object[]]$Values
     )
-$hash = @{}
-$i = 0
-$Keys | foreach {$hash.Add($_,$Values[$i]) ; $i++}
+Begin {}
+Process 
+    {
+        $hash = @{}
+        $i = 0
+        $Keys | foreach {$hash.Add($_,$Values[$i]) ; $i++}
+    }
+End
+    {
+        $hash
+    }
 }
 
+function Test-History
+{
+<##>
+[cmdletbinding()]
+Param 
+    (
+        [System.Collections.Hashtable]$Pairs,
+        [System.Collections.Hashtable[]]$oldpairs
+    )
+Begin {}
+Process 
+    {
+        Foreach ($o in $oldpairs)
+            {
+                if ($bad) {break}
+                foreach ($p in $Pairs.GetEnumerator())
+                    {
+                        If ($p -in $o.GetEnumerator())
+                            {
+                                $bad = $true
+                                break
+                            }
+                    }
+            }
+    }
+end 
+    {
+        if ($bad) {$true}
+        else {$false}
+    }
+}
