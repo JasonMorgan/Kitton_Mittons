fc12b0a05b9da8b6eddfaf94e4e9ecb38a4eb237
jandreacola <julie@andreacola.com>
2014-01-18 16:09:55 -0500

    Basic input file and 3 sample .csv files
    Sample1.csv is just first names, sample2.csv is full names and email
    addresses, Sample3.csv has priority partners identified with yes

---
+++ Event1/Create-NameObject.ps1
@@ -0,0 +1,58 @@
+﻿<#
+.Synopsis
+   Imports CSV file
+.DESCRIPTION
+   Inputs .csv file
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.INPUTS
+   .csv filepath
+.OUTPUTS
+   array to include fullname, email, and and Priority Partner Status
+
+#>
+function Create-NameObject
+{
+    [CmdletBinding()]
+    Param
+    (
+        #Path to .csv file
+        [Parameter(Mandatory=$true, 
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true, 
+                   Position=0
+                   )]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Filepath
+
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    Write-Verbose "Test file path"
+        do
+        {
+         if(Test-Path $Filepath)
+            {$validatefile = $true}
+         else 
+            {
+            $validatefile = $false
+            $filepath = Read-Host "$Filepath is not valid, Please enter valid file name"
+            }
+         } 
+        until ($validatefile -eq $true)
+
+       $users =  Import-csv $Filepath 
+       $users | Out-GridView
+    }
+    End
+    {
+    }
+}
+
