0e29c4358fd5153f625c4a24f6fa23d9b26624e9
Jason Morgan <jmorgan@f9vs.com>
2014-01-26 18:48:07 -0500

    Updated framework
    ok, no need to compare any data, everyone please read the criteria doc
    before creating your extension script.

--- Event2/Root/AuditMod.ps1
+++ Event2/Root/AuditMod.ps1
@@ -1,4 +1,5 @@
 <#
+
 .SYNOPSIS
 Master script that launches registered extension scripts, stores data on central share, and sends alerts if necessary
 
@@ -12,4 +13,37 @@ For the 2014 Winter Scripting Games
 Version 1.0
 Created on: 1/26/2014
 Last Modified: 1/26/2014
+
+***One  outstanding item will be a switch to allow you to set schedules for the various extensions.  Seems to be a requirement for this task.
+
 #>
+
+#region Initialize
+#load config file
+#endregion Initialize
+
+#region RunScripts
+#Add progress bar
+#
+#store each result set in variable
+#one job per script
+#watch for errors
+#endregion RunScripts
+
+#region TestShare
+#be able to start network watcher job
+#be able to switch to local store on failure
+#endregion TestShare
+
+
+#region CheckKeyfile
+#endregion CheckKeyfile
+
+#region HTMLReport
+#endregion HTMLReport
+
+#region XMLReport
+#endregion XMLReport
+
+#region SendNotification
+#endregion SendNotification
---
+++ Event2/Root/Encrypt.ps1
@@ -0,0 +1,17 @@
+<#
+
+.SYNOPSIS
+Encryption script
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.NOTES
+Written by the Kitton Mittons
+For the 2014 Winter Scripting Games
+Version 1.0
+Created on: 1/26/2014
+Last Modified: 1/26/2014
+
+#>
