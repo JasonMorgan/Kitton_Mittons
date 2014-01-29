37b77282c77da4239628cab081ef78cf75f99b39
copperreddc <copperred@gmail.com>
2014-01-28 22:56:59 -0500

    Added headers to test procstest.ps
    Made corrections per JasonM's email and added header

--- Event2/Extensions/procstest.ps1
+++ Event2/Extensions/procstest.ps1
@@ -1,1 +1,34 @@
+<#
+
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+.EXAMPLE
+
+.NOTES
+Written by the Kitton Mittons
+For the 2014 Winter Scripting Games
+Version 1.0
+Created on: 1/26/2014
+Last Modified: 1/26/2014
+
+
+#>
+
+Param 
+    (
+        [switch]$Register
+    )
+
+#region ExtensionHeader
+$Name = "Procs"
+$title = "Process Info"
+$format = "Table"
+if ($Register)
+    {
+        Break
+    }
+#endregion ExtensionHeader
 Get-Process | Select-Object name,fileversion,productversion,company 
