69234c48f31e2761a631f34d7db63fbda402b1b2
DeSchvortz <Stephen.j.Schwartz1@gmail.com>
2014-01-30 23:42:02 -0500

    Autorun Locations
    All key locations with comments.  There are three "outliers" at the
    bottom with comments explaining their special nature.

--- Event2/Extensions/reg.ps1
+++ Event2/Extensions/reg.ps1
@@ -35,15 +35,40 @@ if ($Register)
 #region GatherData
 Write-Verbose "Adding HKU"
 New-PSDrive -PSProvider Registry -Root HKEY_USERS -Name HKU
+New-PSDrive -PSProvider Registry -Root HKEY_CLASSES_ROOT -Name HKCR
 $keys = @(
-        "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
-        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServices"
-        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce"
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-        "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce\Setup"
-
+        "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run" #All values in this key are executed#
+        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunOnce" #All values in this key are executed, and then their autostart reference is deleted#
+        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServices" #All values in this key are executed as services#
+        "HKLM:\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce" #All values in this key are executed as services, and then their autostart reference is deleted#
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" #All values in this key are executed#
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce" #All values in this key are executed, and then their autostart reference is deleted#
+        "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce\Setup" #Used only by Setup. Displays a progress dialog box as the keys are run one at a time#
+        "HKU:\Default\Software\Microsoft\Windows\Cur rentVersion\Run" #All values in this key are executed#
+        "HKU:\Default\Software\Microsoft\Windows\Cur rentVersion\RunOnce" #All values in this key are executed, and then their autostart reference is deleted#
+        "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components" #All values in this key are executed#
+        "HKLM:\SYSTEM\CurrentControlSet\services\VxD" #All values in this key are executed#        
+        "HKCR:\vbsfile\shell\open\command" #Executed whenever a .VBS file is run#
+        "HKCR:\vbefile\shell\open\command" #Executed whenever a .VBE file is run#
+        "HKCR:\jsfile\shell\open\command" #Executed whenever a .JS file is run #
+        "HKCR:\jsefile\shell\open\command" #Executed whenever a .JSE file is run#
+        "HKCR:\wshfile\shell\open\command" #Executed whenever a .WSH file is run#
+        "HKCR:\wsffile\shell\open\command" #Executed whenever a .WSF file is run#
+        "HKCR:\exefile\shell\open\command" #Executed whenever a .EXE file is run#
+        "HKCR:\comfile\shell\open\command" #Executed whenever a .COM file is run#
+        "HKCR:\batfile\shell\open\command" #Executed whenever a .BAT file is run#
+        "HKCR:\scrfile\shell\open\command" #Executed whenever a .SCR file is run#
+        "HKCR:\piffile\shell\open\command" #Executed whenever a .PIF file is run#
+        "HKLM:\System\CurrentControlSet\Services" #Services marked to startup automatically are executed before user login#
+        "HKLM:\System\CurrentControlSet\Services\Winsock2\Parameters\Protocol_Catalog\Catalog_Entries" #Layered Service Providers, executed before user login#
+        "HKLM:\System\Control\WOW\cmdline" #Executed when a 16-bit Windows executable is executed#
+        "HKLM:\System\Control\WOW\wowcmdline" #Executed when a 16-bit DOS application is executed#
+        "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Userinit" #Executed when a user logs in#
+        "HKLM:\Software\Microsoft\Windows\CurrentVersion\ShellServiceObjectDelayLoad" #Executed by explorer.exe as soon as it has loaded#
+        "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\run" #Executed when the user logs in#
+        "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\Windows\load" #Executed when the user logs in#
+        "HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\run" #Subvalues are executed when Explorer initialises#
+        "HKLM\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\run" #Subvalues are executed when Explorer initialises#
     )
 foreach ($k in $keys)
     {
@@ -57,3 +82,7 @@ foreach ($k in $keys)
     }
 
 #region GatherData
+
+        "HKCU:\Control Panel\Desktop" #the "SCRNSAVE.EXE" value is the only autorun value#
+        "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" # the "sHELL" value is the only autorun Value#
+        "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" #the "BootExecute" value is the only autorun value#
