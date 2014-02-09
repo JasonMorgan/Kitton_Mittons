4079f35a3c26e2983827482a8bb3d71871e50052
JasonMorgan <jmorgan@f9vs.com>
2014-02-09 17:28:38 -0500

    Initial testing
    Made some XML files for testing and wrote the first couple of functions
    we will need.

---
+++ Event4/Monitoring.psm1
@@ -0,0 +1,105 @@
+﻿<##>
+
+Function New-XMLConfig
+{
+<#
+
+#>
+Param
+    (
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('Server')]
+        [string]$ComputerName,
+        
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('IP')]
+        [ipaddress]$IPAddress,
+        
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('CPU')]
+        [ValidateSet('True','False')]
+        [string]$MonitorCPU,
+        
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('RAM')]
+        [ValidateSet('True','False')]
+        [string]$MonitorRam,
+        
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('Disk')]
+        [ValidateSet('True','False')]
+        [string]$MonitorDisk,
+        
+        #
+        [Parameter(Mandatory,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+        [Alias('Network')]
+        [ValidateSet('True','False')]
+        [string]$MonitorNetwork
+    )
+Begin
+{
+[xml]$xml = @"
+<?xml version="1.0" encoding="utf-8"?>
+<DRSmonitoring xmlns="http://schemas.drsmonitoring.org/metadata/2013/11">
+  <Server> 
+    <Name></Name> 
+    <IPAddress></IPAddress>
+  </Server>
+  <Monitoring>
+    <MonitorCPU></MonitorCPU>
+    <MonitorRAM></MonitorRAM>
+    <MonitorDisk></MonitorDisk>
+    <MonitorNetwork></MonitorNetwork>
+  </Monitoring>
+</DRSmonitoring>
+"@
+}
+Process
+{
+$xml.DRSmonitoring.Server.Name = $ComputerName
+$xml.DRSmonitoring.Server.IPAddress = "$($IPAddress.IPAddressToString)"
+$xml.DRSmonitoring.Monitoring.MonitorCPU = $MonitorCPU
+$xml.DRSmonitoring.Monitoring.MonitorRAM = $MonitorRam
+$xml.DRSmonitoring.Monitoring.MonitorDisk = $MonitorDisk
+$xml.DRSmonitoring.Monitoring.MonitorNetwork = $MonitorNetwork
+$xml
+}
+}
+
+Function out-XMLFile
+{
+<##>
+Param
+    (
+        #
+        [Parameter(Mandatory)]
+        [xml]$XML,
+        
+        #
+        [Parameter(Mandatory)]
+        [ValidateScript({ Test-path -PathType Container -Path (Split-Path -Parent -path $_) })]
+        [string]$path
+    )
+Begin {}
+Process 
+{
+    $XML.Save($path)
+}
+}
